0e33f6f49548907e3864e7cbd24a943189d9b292
18348086338 <xiaohan0k@163.com>
2018-09-14 16:26:45 +0800

    修复轮播图并完善

--- www/static/js/module/slider.js
+++ www/static/js/module/slider.js
@@ -1,9 +1,23 @@
 var mySwiper = new Swiper('.swiper-container', {
   autoplay: true, // 可选选项，自动滑动
   slidesPerView: 'auto',
+  effect: 'fade',
   observer: true, // 修改swiper自己或子元素时，自动初始化swiper
   observeParents: true, // 修改swiper的父元素时，自动初始化swiper
   pagination: {
-    el: '.swiper-pagination'
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    }
+  },
+  on: {
+    init: function() {
+      swiperAnimateCache(this); // 隐藏动画元素
+      swiperAnimate(this); // 初始化完成开始动画
+    },
+    slideChangeTransitionEnd: function() {
+      swiperAnimate(this); // 每个slide切换结束时也运行当前slide动画
+    }
   }
 });
